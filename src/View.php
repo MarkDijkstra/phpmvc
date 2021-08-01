bcc3e0b384a6c4e71f59d0fddd6c08d4cf6821cd
Mark Dijkstra <markdijkstra.eu@gmail.com>
2021-08-01 11:27:17 +0200

    Added new View class

---
+++ src/View.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Core;
+
+class View 
+{
+
+    /**
+     * The render method.
+     *
+     * @param string $view A string representation of page template.
+     * @param array $params A array of the controller variables.
+     * @return void
+     */
+    static function render(string $view, array $params): void 
+    {
+        extract($params, EXTR_SKIP);
+        $content = APPLICATION_PATH . "/app/Views/Page/$view.php";
+
+        if (is_readable($content)) {
+            require_once APPLICATION_PATH . "/app/Views/Base.php";            
+        } else {            
+            throw new \Exception("View $view not found");
+        }
+    }
+
+    /**
+     * The error render method.
+     * 
+     * @param array $params A array of the controller variables.
+     * @return array  The route details.
+     */
+    static function renderError(array $params): void 
+    {
+        extract($params, EXTR_SKIP);
+        $content = APPLICATION_PATH . "/App/Views/Error.php"; 
+
+        if (is_readable($content)) {            
+            require_once $content;            
+        } else {            
+            throw new \Exception("View ERROR not found");
+        }
+    }
+}
