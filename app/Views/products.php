0d6e76d3bc9a2b2c936d53872683d5bd94238694
Mark Dijkstra <markdijkstra.eu@gmail.com>
2021-08-04 12:13:25 +0200

    Visual output of the products

--- app/Views/products.php
+++ app/Views/products.php
@@ -4,9 +4,17 @@
     </h1>
     <p>
     Show a list of all products here...
-    <br/> 
-    <br/> 
-    <br/>
-    <?php print_r($products)?>
     </p>
+    <div id="products">
+    <?php 
+        foreach($products as $key => $value) {
+           echo '<div>';
+           echo '<p><strong>Id:</strong> '.$value['id'].'</p>';
+           echo '<p><strong>Name:</strong> '.$value['name'].'</p>';
+           echo '<p><strong>Category:</strong> '.$value['category'].'</p>';
+           echo '<p><strong>Price:</strong> '.$value['price'].'</p>';
+           echo '</div>';
+        }
+    ?>
+    </div>
 </div>
