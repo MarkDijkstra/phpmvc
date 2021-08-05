804d62a9fbe8bb3e6d0aecbd53840aec39608ab4
Mark Dijkstra <markdijkstra.eu@gmail.com>
2021-08-05 12:20:40 +0200

    Extended PDO options

--- core/Database.php
+++ core/Database.php
@@ -22,10 +22,12 @@ class Database extends Query
         if ($this->db === null) {            
             $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8';
 
-            $options = [
+            $options = array(
                 PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ];
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                PDO::ATTR_CASE => PDO::CASE_LOWER
+             );
             
             $this->db = new PDO($dsn, DB_USER, DB_PASSWORD, $options);
         }
