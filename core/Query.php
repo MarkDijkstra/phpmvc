bb52ccd26423c957df821db0a1872ce559a91334
Mark Dijkstra <markdijkstra.eu@gmail.com>
2021-08-06 17:12:55 +0200

    Updated Query class

--- core/Query.php
+++ core/Query.php
@@ -4,52 +4,94 @@ namespace Core;
 
 use PDO;
 
-class Query
+class Query extends Database
 {
  
     private $table;
-    private $dbh;
     private $stmt;
+    private $from;
+    private $columns;
 
     /**
      * The model construct
      * 
      * @param $table the name of the table
      */
-    public function __construct($table_name) 
+    public function __construct() 
     {
-        $this->table = $table_name;
+       parent::__construct();
+    }
+        
+    /**
+     * Method query
+     *
+     * @param $sql Query to run
+     * @return object
+     */
+    private function query($sql) : object
+    {
+        $this->stmt = $this->db->prepare($sql);
+        $this->execute();
+        return $this;
     }
     
-
-    public function query($query = null): void
+    /**
+     * Method select
+     *
+     * @param $columns The columns that are selected
+     * @return object
+     */
+    public function select($columns = '*') : object
     {
-        $this->stmt = $this->db->prepare($query);
+        if ($columns) { 
+            // todo: filtering and options
+        }
+        
+        $this->columns = $columns;
+        return $this;
+    }
+    
+    /**
+     * Method from
+     *
+     * @param String $table Select the table to retrive the data from
+     * @return object
+     */
+    public function from(String $table) : object
+    {
+        $this->table = $table;
+        return $this;
     }
 
-    public function execute()
+    public function where()
     {
-        $this->stmt->execute();
+        
     }
-
-
 
     /**
      * Method all
      *
-     * @param $table the name of the table
      * @return array
      */    
-    public function all($table = null): array 
+    public function all() : array
     {
-        if(!$table) {
-           $table = $this->table;
-        }
-        
-        $query = $this->db->prepare('SELECT * FROM '.$table);
-        $query->execute();
+        $this->query('SELECT '.$this->columns.' FROM '.$this->table);
+        $this->stmt  = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        return $query->fetchAll(PDO::FETCH_ASSOC);
+        return $this->stmt;
+    }
+
+    /**
+     * Method one
+     *
+     * @return array
+     */  
+    public function one() : array
+    {
+        $this->execute();
+        $this->stmt = $this->stmt->fetch(PDO::FETCH_ASSOC);
+        
+        return $this->stmt;
     }
 
     public function find()
@@ -61,41 +103,54 @@ class Query
     {
         
     }
-
+    
     public function update()
     {
-        
+       
     }
 
     public function delete()
     {
         
     }
-
     
+    public function first()
+    {
+
+    }
+
+    public function last()
+    {
+
+    }
+
+    public function toObject()
+    {
+
+    }
+
     /**
-     * Method dropTable
+     * Method toArray
      *
-     * @param $tableName $tableName Name of the table.
-     *
-     * @return void
+     * @return array
      */
+    public function toArray() : array
+    {
+        return json_decode(json_encode((array) $this->stmt), true);
+    }
+
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
+    
     public function dropTable($tableName = '')
     {
-        $query = "DROP TABLE $tableName";
-        $result = '';
 
-        if ($result) {
-            $this->result = "Table has been dropped";
-            return true;
-        }
-
-        return false;
     }
 
     public function sql()
     {
         
     }
-
 }
