c6d5d6abafdc7b745b75afe996031645becbe42c
Mark Dijkstra <markdijkstra.eu@gmail.com>
2021-08-13 11:43:21 +0200

    Removed unused code

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,5 @@
 use Core\Route;
 
 Route::get('/', ['HomeController','index']);
-Route::get('products/{id}{category}/{test}', ['ProductController','index']);
+Route::get('products/{id}', ['ProductController','index']);
 Route::get('404', ['NotfoundController','index']);
-
-return [
-    "GET" => [
-        "/" => "HomeController@index",
-        "products" => "ProductController@index",
-        "404" => "NotfoundController@index"
-    ]
-];
-
-
-
-
-
-
